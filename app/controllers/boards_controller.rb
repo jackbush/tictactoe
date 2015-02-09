db814b1bfde646096753c18000f115f3125a1ffe
Jack Bush <bush.jack@gmail.com>
2015-02-09 15:20:18 +0000

    added crud functions to game and board controllers

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,24 +1,40 @@
 class BoardsController < ApplicationController
 
   def index
+    @boards = Board.all
   end
 
   def show
+    @board = Board.find(params[:id])
   end
 
   def new
+    @board = Board.new
   end
 
   def create
+    Board.create(board_params)
+    redirect_to boards_path
   end
 
   def edit
+    @board = Board.find(params[:id])
   end
 
   def update
+    @board = Board.find(params[:id])
+    @board.update(board_params)
+    redirect_to boards_path
   end
 
   def destroy
+    Board.find(params[:id]).destroy
+    redirect_to boards_path
+  end
+
+  private
+  def board_params
+    params.require(:board).permit(:game_id, {boards_players_attributes: [:player_id, :winner]})
   end
   
 end
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,24 +1,40 @@
 class GamesController < ApplicationController
 
   def index
+    @games = Game.all
   end
 
   def show
+    @game = Game.find(params[:id])
   end
 
   def new
+    @game = Game.new
   end
 
   def create
+    Game.create(game_params)
+    redirect_to games_path
   end
 
   def edit
+    @game = Game.find(params[:id])
   end
 
   def update
+    @game = Game.find(params[:id])
+    @game.update(game_params)
+    redirect_to games_path
   end
 
   def destroy
+    Game.find(params[:id]).destroy
+    redirect_to games_path
+  end
+
+  private
+  def game_params
+    params.require(:game).permit(:name)
   end
   
 end
