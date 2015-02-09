ea60e6c5035bf62c164ffbf891b105751d02f190
Jack Bush <bush.jack@gmail.com>
2015-02-09 15:02:38 +0000

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+
+  resources :games
+  resources :boards
+  resources :players
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+
+  root 'boards#index'
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
