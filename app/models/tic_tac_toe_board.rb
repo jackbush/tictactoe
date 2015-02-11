396d86c2136a65f7b07deb72d42d44bf4400cb23
Jack Bush <bush.jack@gmail.com>
2015-02-11 15:00:19 +0000

    refactored play method

--- app/models/tic_tac_toe_board.rb
+++ app/models/tic_tac_toe_board.rb
@@ -5,13 +5,17 @@ class TicTacToeBoard < ActiveRecord::Base
 
   @winning_combinations = [['0','1','2'],['3','4','5'],['6','7','8'],['0','3','6'],['1','4','7'],['2','5','8'],['0','4','8'],['2','4','6']]
 
-  def self.computer_move(game)
-    available_squares = Array.new
-    available_squares << game.board
-    available_squares.flatten!
-    available_squares.delete('x')
-    available_squares.delete('o')
-    move = available_squares.sample.to_i
+  def self.available_squares(board)
+    result = Array.new
+    result << board
+    result.flatten!
+    result.delete('x')
+    result.delete('o')
+    result
+  end
+
+  def self.computer_move(board)
+    self.available_squares(board).sample.to_i
   end
 
   def self.board_update_x(game, square)
@@ -33,29 +37,21 @@ class TicTacToeBoard < ActiveRecord::Base
   end
 
   def self.check_draw(board)
-    result = Array.new
-    result << board
-    result.flatten!
-    result.delete('x')
-    result.delete('o')
-    result.empty?
+    self.available_squares(board).empty?
   end
 
   def self.play(game, user_square)
     game.board = self.board_update_x(game, user_square)
     game.p1_squares << user_square.to_s
-
     game.finished = self.check_win(game.p1_squares) || self.check_draw(game.board)
     return game if game.finished == true
-
     # if p2_id == nil
-      comp_square = self.computer_move(game)
+      comp_square = self.computer_move(game.board)
       game.board = self.board_update_o(game, comp_square)
       game.p2_squares << comp_square.to_s
     # else
       #wait for p2 move
     # end
-
     game.finished = self.check_win(game.p2_squares) || self.check_draw(game.board)
     game
   end
