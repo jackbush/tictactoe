531c00f813ba7bfaf241bfac1702bcf317eb9b66
Jack Bush <bush.jack@gmail.com>
2015-02-11 08:48:09 +0000

    commented in today's modifications

--- app/models/tic_tac_toe_board.rb
+++ app/models/tic_tac_toe_board.rb
@@ -15,22 +15,28 @@ class TicTacToeBoard < ActiveRecord::Base
   end
 
   def self.computer_move(game)
-    a = Array.new
-    a << game.board
-    a.flatten!
-    a.delete('X')
-    a.delete('O')
-    square = a.sample.to_i
+    available_squares = Array.new
+    available_squares << game.board
+    available_squares.flatten!
+    available_squares.delete('X')
+    available_squares.delete('O')
+    square = available_squares.sample.to_i
   end
 
   def self.play(game, user_square)
     # binding.pry
     game.board = self.board_update_x(game, user_square)
     game.p1_squares << user_square
-    comp_square = self.computer_move(game)
-    game.board = self.board_update_o(game, comp_square)
-    game.p2_squares << comp_square
-    game
+    #check for winner
+    #count if turn < 8
+    #if board contains numbers
+      comp_square = self.computer_move(game)
+      game.board = self.board_update_o(game, comp_square)
+      game.p2_squares << comp_square
+      #check for winner
+      game
+    #else
+    #draw
   end
 
 end
