a5f41c29ed212efa41385af2796f84143c729f35
Jack Bush <bush.jack@gmail.com>
2015-02-11 17:34:07 +0000

    added role, set default to basic

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,8 +11,3 @@ TicTacToeBoard.delete_all
 Player.delete_all
 
 Game.create(name: "Tic Tac Toe", instructions: "On a 3x3 grid, claim three squares in a row.")
-
-Player.create(username: "test1", games_played: 43, games_won: 14)
-Player.create(username: "test2", games_played: 3, games_won: 1)
-Player.create(username: "test3", games_played: 12, games_won: 11)
-Player.create(username: "test4", games_played: 67, games_won: 54)
