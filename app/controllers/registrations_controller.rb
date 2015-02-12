a6732b3249910e2f836c61a25598db5d06835862
Jack Bush <bush.jack@gmail.com>
2015-02-12 17:30:29 +0000

    changed from avatar to gravatar

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,11 +3,17 @@ class RegistrationsController < Devise::RegistrationsController
   private
  
   def sign_up_params
-    params.require(:player).permit(:username, :avatar, :email, :password, :password_confirmation)
+    params.require(:player).permit(:username, :email, :password, :password_confirmation)
   end
  
   def account_update_params
-    params.require(:player).permit(:username, :avatar, :email, :password, :password_confirmation, :current_password)
+    params.require(:player).permit(:username, :email, :password, :password_confirmation, :current_password)
   end
   
 end
+
+  # :avatar
+  # <div class="field">
+  #   <%= f.label :avatar %><br />
+  #   <%= f.file_field :avatar %>
+  # </div>
--- app/models/player.rb
+++ app/models/player.rb
@@ -5,7 +5,11 @@ class Player < ActiveRecord::Base
   has_many :tic_tac_toe_boards
 
   validates :username, presence: true, uniqueness: true
+
   mount_uploader :avatar, AvatarUploader
+
+  include Gravtastic
+  gravtastic size: 120, default: "identicon"
 
   def admin?
     self.role == 'admin'
