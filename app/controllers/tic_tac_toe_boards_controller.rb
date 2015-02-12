694e2d624b0db5aa912cac92487525ba9c1b6eff
Jack Bush <bush.jack@gmail.com>
2015-02-12 09:29:33 +0000

    p1 registration

--- app/controllers/tic_tac_toe_boards_controller.rb
+++ app/controllers/tic_tac_toe_boards_controller.rb
@@ -15,7 +15,7 @@ class TicTacToeBoardsController < ApplicationController
   def create
     game = TicTacToeBoard.create
     game.board << 0 << 1 << 2 << 3 << 4 << 5 << 6 << 7 << 8
-    game.a_player = 20
+    game.a_player = current_player
     game.save
     redirect_to edit_tic_tac_toe_board_path(game)
   end
